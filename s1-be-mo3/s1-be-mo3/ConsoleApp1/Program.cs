e5824d6ba77c94f6f9a47fdca40b1b51c4293042
yaboySLAM <44971734+yaboySLAM@users.noreply.github.com>
2018-11-23 13:56:16 +0100

    test

---
+++ s1-be-mo3/s1-be-mo3/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Voer uw studienummer in: ");
+            int nummer = Convert.ToInt32(Console.Read());
+            Console.Write("voer uw naam in: ");
+            var naam = Console.Read();
+            Console.Write("voer uw achternaam in: ");
+            var achternaam = Convert.ToInt32(Console.Read());
+
+        }
+    }
+}
