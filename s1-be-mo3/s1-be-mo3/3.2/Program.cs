32dcfd2984a1374859c329875eb0bb4e735a5e77
dannyoo7 <dannylanssink@outloo.com>
2018-11-23 14:50:37 +0100

    danny 3.2

--- s1-be-mo3/s1-be-mo3/3.2/Program.cs
+++ s1-be-mo3/s1-be-mo3/3.2/Program.cs
@@ -9,40 +9,58 @@ namespace _3._2
     class Program
     {
         static void Main(string[] args)
+            //vragen
         { Console.Write("datum van de mans ze geboorte dd/mm/yy : ");
             string me = Console.ReadLine();
             Console.Write("datum van de vrouw ze geboorte dd/mm/yy : ");
             string xe = Console.ReadLine();
+            //vragen
+            //man
+            //mijn verjaardag is (input) afgerond
             DateTime myBirthdatee = DateTime.Parse(me);
+            // mijn leeftijd is huidige datum - mijn geboorte datum
             TimeSpan myAGEE = DateTime.Now.Subtract(myBirthdatee);
+            // converter
             double mee = Convert.ToDouble(myAGEE.TotalHours.ToString());
-            Double meee = mee / 24;
-            Double meeee = meee / 365;
-            double man = Math.Truncate(meeee);
+            Double dag1 = mee / 24;
+            double man = Math.Truncate(dag1);
+            //man
+            //vrouw
             DateTime myBirthdate = DateTime.Parse(xe);
             TimeSpan myAGE = DateTime.Now.Subtract(myBirthdate);
             Double xee = Convert.ToDouble(myAGE.TotalHours.ToString());
-            Double xeee = xee / 24;
-            Double xeeee = xeee / 365;
-            double vrouw = Math.Truncate(meeee);
+            Double dag2 = xee / 24;
+            double vrouw = Math.Truncate(dag2);
+            //vrouw
+            //berekeningen
             int antwoord1 = (int)Math.Round(vrouw - man);
             int antwoord2 = (int)Math.Round(man - vrouw);
             if (antwoord1 < 0)
             {
+                Console.Write(antwoord2);
+                Console.ReadKey();
                 double antwoord = antwoord2;
                 Console.WriteLine(antwoord);
                 Console.ReadLine();
+                Console.Write("a1");
+                Console.ReadKey();
             }
             if (antwoord2 < 0) 
             {
-                int antwoord = antwoord2 = antwoord1;
+                Console.Write(antwoord1);
+                Console.ReadKey();
+                int antwoord  = antwoord1;
                 Console.WriteLine(antwoord);
                 Console.ReadLine();
+                Console.Write("a2");
+                Console.ReadKey();
             }
             else
             {
                 int antwoord = (int)Math.Round(man - vrouw );
                 Console.Write(antwoord);
+                Console.ReadKey();
+                Console.Write("else");
                 Console.ReadKey();
             }
         }
