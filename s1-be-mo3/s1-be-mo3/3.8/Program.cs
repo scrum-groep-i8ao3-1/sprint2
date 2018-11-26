9ef86d2b8bfc6721fa4248280d8220906c4a7f98
dannyoo7 <dannylanssink@outloo.com>
2018-11-26 14:29:03 +0100

    updates

---
+++ s1-be-mo3/s1-be-mo3/3.8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht3._8
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(1, 101); // creates a number between 1 and 12
+            int a = (int)Convert.ToInt32(random);
+            Console.WriteLine("Wat is uw voornaam? ");
+            string voornaam = Console.ReadLine();
+            Console.WriteLine("Wat zijn uw tussenvoegsels? ");
+            string tv = Console.ReadLine();
+            Console.WriteLine("Wat is uw achternaam? ");
+            string achternaam = Console.ReadLine();
+            Console.Clear();
+            Console.Write(voornaam +tv+ achternaam+a);
+            Console.ReadLine();
+
+        }
+    }
+}
