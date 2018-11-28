2f68ff920bf23e919b31e2393cfb97dc1d52afab
dannyoo7 <dannylanssink@outloo.com>
2018-11-28 12:20:23 +0100

    3.9 jairo noob

---
+++ s1-be-mo3/s1-be-mo3/3.9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voer uw datum zo in: DD/MM/YYYY  ");
+            string dtm = Console.ReadLine();
+            DateTime datum = DateTime.Parse(dtm);
+            var year = datum.Year;
+            var month = datum.Month;
+            var day = datum.Day;
+            var laatstedag = DateTime.DaysInMonth(year, month);
+            Console.Write(laatstedag);
+            Console.ReadKey();
+        }
+    }
+}
