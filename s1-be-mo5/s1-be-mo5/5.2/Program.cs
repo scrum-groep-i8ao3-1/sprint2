f40c7f084559da85cf6fa778c5fc627adb57143e
dannyoo7 <dannylanssink@outloo.com>
2018-11-28 10:04:33 +0100

    dannys meuk

---
+++ s1-be-mo5/s1-be-mo5/5.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                int skwadraat = i * i ;
+                i kwadraat = Int32.Parse(skwadraat);
+                Console.WriteLine(i+ikwadraat);
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
