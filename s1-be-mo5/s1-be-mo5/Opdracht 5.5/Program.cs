ffd6dc7ed6cd1d159c938fa3a53ec6a41fed45be
Tom Morskieft <0311319@student.rocvantwente.nl>
2018-11-28 11:37:10 +0100

    uptodate

---
+++ s1-be-mo5/s1-be-mo5/Opdracht 5.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deze applicatie laat de uitkomst zien van de getallen 100 t/m 1000 bij elkaar opgeteld.");
+            Console.ReadLine();
+
+            int getal1, getal2;
+
+            getal1 = 100;
+            getal2 = 1000;
+            int totaal = 0;
+
+            for (int i = getal1; i <= getal2; i++)
+            {
+                totaal += i;
+            }
+            Console.WriteLine("De uitkomst van alle getallen bij elkaar opgeteld is: " + totaal);
+            Console.ReadLine();
+        }
+    }
+}
