d7812a8c7c1b106c32987ca0dadbb2e38ad09828
yaboySLAM <44971734+yaboySLAM@users.noreply.github.com>
2018-11-28 10:01:34 +0100

    pullen voor opdrachten van timur

---
+++ s1-be-mo5/s1-be-mo5/5.17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // revers string 
+            string input = string.Empty;
+            string reverse = string.Empty;
+
+            //de invoer van de gebruiker 
+            Console.WriteLine("schrijf hier een woord of een zin:  ");
+            input = Console.ReadLine();
+
+            for (int e = input.Length - 1; e >= 0; e--)
+                reverse += input[e];
+
+            // de uitvoer reverse woord of zin 
+            Console.WriteLine("dit is jouw woord of zin achterstevoren: " + reverse);
+            Console.ReadKey();
+
+        }
+    }
+}
