0eec98b5d7b5553c9a63fe1111d09813da1ca1f7
jairokooistra <44971993+jairokooistra@users.noreply.github.com>
2018-11-29 14:10:30 +0100

    jairo

--- s1-be-mo3/s1-be-mo3/3.2/Program.cs
+++ s1-be-mo3/s1-be-mo3/3.2/Program.cs
@@ -4,44 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Opdracht_8_test
+namespace ConsoleApp12
 {
     class Program
     {
-        static int CountChars(string value)
-        {
-            int result = 0;
-            bool lastWasSpace = false;
-
-            foreach (char c in value)
-            {
-                if (char.IsWhiteSpace(c))
-                {
-
-                    if (lastWasSpace == false)
-                    {
-                        result++;
-                    }
-                    lastWasSpace = true;
-                }
-                else
-                {
-                    result++;
-                    lastWasSpace = false;
-                }
-            }
-            return result;
-        }
-
         static void Main(string[] args)
         {
-            Console.Write("Voer je volledige naam in: ");
-            string zin = Console.ReadLine();
+            Console.WriteLine("geef nummer van de maand");
+            int maand = Convert.ToInt32(Console.ReadLine());
+            if (maand == 1)
+            {
+                Console.WriteLine("31 dagen");
+            }
+            else if (maand == 2)
+            {
+                Console.WriteLine("28");
+            }
+            else if (maand == 3)
+            {
+                Console.WriteLine("31");
+            }
+            else if (maand == 4)
+            {
+                Console.WriteLine("30");
+            }
+            else if (maand == 5)
+            {
+                Console.WriteLine("31");
+            }
+            else if (maand == 6)
+            {
+                Console.WriteLine("30");
+            }
+            else if (maand == 7)
+            {
+                Console.WriteLine("31");
+            }
+            else if (maand == 8)
+            {
+                Console.WriteLine("31");
+            }
+            else if (maand == 9)
+            {
+                Console.WriteLine("30");
+            }
+            else if (maand == 10)
+            {
+                Console.WriteLine("31");
+            }
+            else if (maand == 11)
+            {
+                Console.WriteLine("30");
+            }
+            else if (maand == 12)
+            {
+                Console.WriteLine("31");
+            }
+            else
+            {
+                Console.WriteLine("dat is geen datum");
 
-            zin = zin.Replace(" ", string.Empty);
-            int count = CountChars(zin);
-            Console.WriteLine(count);
+
+            }
             Console.ReadKey();
         }
     }
 }
+
