c2e30bde5102526e7f9c9ae069e97b887c0a3b7d
dannyoo7 <dannylanssink@outloo.com>
2018-11-30 11:07:15 +0100

    test

---
+++ s1-be-mo4/s1-be-mo4/4.12.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Voer uw datum zo in: DD/MM/YYYY  ");
+            string dtm = Console.ReadLine();
+            DateTime datum = DateTime.Parse(dtm);
+            var year = datum.Year;
+            var month = datum.Month;
+            var day = datum.Day;
+            DateTime monthStart = new DateTime(year, month, 1);
+            string myStringDate = monthStart.ToString();
+            string xx = System.Threading.Thread.CurrentThread.CurrentUICulture.DateTimeFormat.GetDayName(DateTime.Parse(myStringDate).DayOfWeek);
+            Console.WriteLine(xx);
+            Console.ReadLine();
+        }
+    }
+}
